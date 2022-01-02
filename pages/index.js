e53c70e260685b20489e3579ed0de3ff4b5e79b8
Ahmad Alfan <aalf4nz@gmail.com>
2022-01-02 14:06:10 +0700

    Replace count down checking using global state

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,17 @@ import MainLayout from '../layouts/MainLayout';
 import Form from '../components/Form';
 import Poster from '../components/Poster';
 import CountDown from '../components/CountDown';
-import { useState, useEffect } from 'react';
-import { useWebinarDate } from '../hooks/useRegistration';
+import { useCountDown } from '../store/countDown';
 
 export default function Home() {
-    const { data: webinarDate } = useWebinarDate();
-
-    const [showForm, setShowForm] = useState(false);
-    useEffect(() => {
-        setShowForm(new Date() < new Date(webinarDate));
-    }, [webinarDate]);
+    const isOutOfTime = useCountDown(state => state.isOutOfTime);
 
     return (
         <MainLayout>
             <Poster imagePath={'/poster.png'} />
             <div className='flex flex-col gap-8 md:gap-4 w-full max-w-xl'>
                 <CountDown />
-                {showForm && <Form />}
+                {!isOutOfTime && <Form />}
             </div>
         </MainLayout>
     );
