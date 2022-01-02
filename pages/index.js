4b143e2adc52876d79678b3316e5280d5c0c1178
Ahmad Alfan <aalf4nz@gmail.com>
2022-01-02 14:17:49 +0700

    Add head title and meta data

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,27 @@ import Form from '../components/Form';
 import Poster from '../components/Poster';
 import CountDown from '../components/CountDown';
 import { useCountDown } from '../store/countDown';
+import Head from 'next/head';
 
 export default function Home() {
     const isOutOfTime = useCountDown(state => state.isOutOfTime);
 
     return (
-        <MainLayout>
-            <Poster imagePath={'/poster.png'} />
-            <div className='flex flex-col gap-8 md:gap-4 w-full max-w-xl'>
-                <CountDown />
-                {!isOutOfTime && <Form />}
-            </div>
-        </MainLayout>
+        <>
+            <Head>
+                <title>Webinar Artificial Intellignce IMV Laboratory</title>
+                <meta
+                    name='description'
+                    content='Ayo daftarkan dirimu sebagai peserta webinar gratis tentang Artificial Intelligence (AI) dari IMV Laboratory. Dapatkan wawasan mengenai prospek karir sebagai alumni IMV Laboratory!'
+                />
+            </Head>
+            <MainLayout>
+                <Poster imagePath={'/poster.png'} />
+                <div className='flex flex-col gap-8 md:gap-4 w-full max-w-xl'>
+                    <CountDown />
+                    {!isOutOfTime && <Form />}
+                </div>
+            </MainLayout>
+        </>
     );
 }
