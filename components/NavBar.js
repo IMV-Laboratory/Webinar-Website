efc2b3eecd6fc3ab39a86d8249089868e493d044
Ahmad Alfan <aalf4nz@gmail.com>
2022-01-28 08:58:21 +0700

    Add navigation button to toggle between forms

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,22 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+const toggleMenu = {
+    '/': {
+        title: 'Form Kehadiran',
+        path: '/attendance',
+    },
+    '/attendance': {
+        title: 'Form Pendaftaran',
+        path: '/',
+    },
+};
 
 const NavBar = () => {
+    const router = useRouter();
+
     return (
-        <div className='pt-8 px-8 w-full'>
+        <div className='flex justify-between items-center p-4 md:pt-8 md:px-8 w-full'>
             <div className='flex gap-4 justify-center md:justify-start'>
                 <Image
                     src='/Logo T_White Horizontal 1.png'
@@ -12,6 +26,11 @@ const NavBar = () => {
                     alt='IMV Logo'
                 />
             </div>
+            <button
+                onClick={() => router.push(toggleMenu[router.pathname].path)}
+                className='bg-white bg-opacity-25 hover:bg-opacity-10 px-4 py-3'>
+                {toggleMenu[router.pathname].title}
+            </button>
         </div>
     );
 };
